3b69debd4c9bcf94170eb8f59f8f19be183feeb0
djilali <derndjilali@gmail.com>
2024-08-07 23:26:18 +0100

    cstum header

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html 
+    <?php
+        // to get direction and language
+        language_attributes();
+    ?>
+>
+<head>
+    <meta <?php bloginfo('charset')?>>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name')?></title>
+    <?php 
+        wp_head();
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
